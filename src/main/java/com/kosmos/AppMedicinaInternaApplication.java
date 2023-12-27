5a4966628d23864ffec257726cd47dffafecb381
Marcos Manuel Arreguín Pérez <marcosmap@192.168.1.12>
2023-12-27 16:12:51 -0600

    initial commit

---
+++ src/main/java/com/kosmos/AppMedicinaInternaApplication.java
@@ -0,0 +1,13 @@
+package com.kosmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMedicinaInternaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMedicinaInternaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosmos/AppMedicinaInternaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMedicinaInternaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
